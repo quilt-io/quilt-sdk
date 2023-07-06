9047baf25c9cad51291c127f4bbbf4ab3bea45e0
jessicapetrochuk <jesspe@gmail.com>
2023-07-05 19:30:32 -0700

    Initial commit

---
+++ Sources/quilt-sdk/QuiltClient.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jessica Petrochuk on 7/5/23.
+//
+
+import Foundation
+
+class APIClient {
+    private let apiKey: String
+
+    init(apiKey: String) {
+        self.apiKey = apiKey
+    }
+}
