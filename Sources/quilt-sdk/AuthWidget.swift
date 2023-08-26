1c2b1ab15e7313bece5a06211526a6cb66f6b447
jessicapetrochuk <jesspe@gmail.com>
2023-08-26 18:25:23 -0400

    Clean up

--- Sources/quilt-sdk/AuthWidget.swift
+++ Sources/quilt-sdk/AuthWidget.swift
@@ -10,11 +10,9 @@ import Foundation
 @available(macOS 13, iOS 16.0, *)
 public struct AuthWidget: View {
     @State private var isConnectedToHealthKit = false
-    @Binding var showWidget: Bool // Pass in a binding to showModal
+    @Binding var showWidget: Bool
     
     private let quiltClient: QuiltClient
-    
-    // TODO: figure out where the user ID should be passed
     private let userId: String
     
 
@@ -39,45 +37,8 @@ public struct AuthWidget: View {
                     // TODO: fix this later, predefining to simplify initial implementation
                     Task {
                         let typesToRead = [dataTypes.heartRate.quantityType]
-                        
-                        // TODO: need to change to take the source id
                         await quiltClient.getUserData(userId: userId, typesToRead: typesToRead)
                     }
-//                    let healthKitInterface = HealthKitInterface(typesToRead: typesToRead)
-//                    healthKitInterface.requestAuthorization { success in
-//                        if success {
-//                            isConnectedToHealthKit = true
-//                            print("Connected to HeslthKit")
-//                            print("Starting URL session")
-                    
-//                            let session = URLSession.shared
-//                            let baseURL = URL(string: api)!
-//                            let userId = URLQueryItem(name: "user_id", value: userId)
-//                            let sourceId = URLQueryItem(name: "source_id", value: sourceId)
-//                            let url = baseURL.appending(queryItems: [
-//                                userId,
-//                                sourceId
-//                            ])
-//                            var request = URLRequest(url: url)
-//
-//                            request.httpMethod = "POST"
-//                            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-//                            request.addValue(apiKey, forHTTPHeaderField: "Authorization")
-//
-//                            let task = session.dataTask(with: request) { (data, response, error) in
-//                                if let error = error {
-//                                    print(error)
-//                                    return
-//                                }
-//                                print(response)
-//                                print(data)
-//                            }
-//                            task.resume()
-//                        }
-//                    }
-                
-//                    QuiltClient(apiKey: apiKey)
-//                    QuiltClient.getUserData(<#T##self: QuiltClient##QuiltClient#>)
                 }
             }
             .padding()
@@ -88,7 +49,7 @@ public struct AuthWidget: View {
             Spacer()
 
             Button("Done") {
-                showWidget = false // Set showModal to false using the binding
+                showWidget = false
             }
             
             Spacer()
--- Sources/quilt-sdk/QuiltClient.swift
+++ Sources/quilt-sdk/QuiltClient.swift
@@ -8,7 +8,6 @@
 import Foundation
 import HealthKit
 
-// Class that allows for easier interaction with the Quilt API
 @available(macOS 13, iOS 16.0, *)
 public class QuiltClient {
     private let apiKey: String
@@ -66,18 +65,13 @@ public class QuiltClient {
     
     private func sendData(jsonData: Data, tableName: String) {
         let session = URLSession.shared
-//        let apiUrl = URL(string: "\(api)/data?table_name=\(tableName)&source_id=test_source")!
-        let apiUrl = URL(string: "\(api)/data?table_name=heart-rate&source_id=test_source")!
+        let apiUrl = URL(string: "\(api)/data?table_name=heart-rate&source_id=\(sourceId)")!
         print(apiUrl)
         var request = URLRequest(url: apiUrl)
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpBody = jsonData
         request.addValue(apiKey, forHTTPHeaderField: "Authorization")
-        
-        // TODO: add source id to this request
-        print(jsonData)
-        print(request)
 
         let task = session.dataTask(with: request) { (data, response, error) in
             if let error = error {
@@ -106,7 +100,6 @@ public class QuiltClient {
     public func getUserData(userId: String, typesToRead: [HKObjectType]) async {
         let healthKitInterface = HealthKitInterface(typesToRead: typesToRead)
         
-        // Request authorization for the user data
         healthKitInterface.requestAuthorization { success in
             if success {
                 print("Connected to HealthKit")
@@ -121,13 +114,12 @@ public class QuiltClient {
         
         for (sampleType, sampleArray) in samples {
             if let jsonData = healthKitInterface.transformData(userId: userId, samples: sampleArray) {
-                print("Transformed!")
-                if let jsonString = String(data: jsonData, encoding: .utf8) {
-                        print(jsonString)
-                    } else {
-                        print("Failed to convert JSON data to string")
-                    }
-                print(jsonData)
+//                print("Transformed!")
+//                if let jsonString = String(data: jsonData, encoding: .utf8) {
+//                        print(jsonString)
+//                    } else {
+//                        print("Failed to convert JSON data to string")
+//                    }
                 sendData(jsonData: jsonData, tableName:sampleType)
             } else {
                 print("Failed to get JSON data")
