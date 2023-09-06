b9c0632926e454bbe2041b99732abccbc496ef1e
jessicapetrochuk <jesspe@gmail.com>
2023-09-05 21:33:30 -0400

    Print statement for debugging

--- Sources/quilt-sdk/QuiltClient.swift
+++ Sources/quilt-sdk/QuiltClient.swift
@@ -116,6 +116,7 @@ public class QuiltClient {
         
         for (sampleType, sampleArray) in samples {
             if let jsonData = healthKitInterface.transformData(userId: userId, samples: sampleArray) {
+                print(jsonData)
                 sendData(jsonData: jsonData)
             } else {
                 print("Failed to transform data")
